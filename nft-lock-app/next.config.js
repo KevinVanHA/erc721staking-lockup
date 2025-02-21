77217bdbf3d8af7835e6c886153e81c635f0b664
Kevin Van Haesendonck <118561655+KevinVanHA@users.noreply.github.com>
2025-02-21 06:24:12 -0500

    v1

--- nft-lock-app/next.config.js
+++ nft-lock-app/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "",
-};
+  swcMinify: true,
+  env: {
+    NEXT_PUBLIC_TEMPLATE_CLIENT_ID: process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- nft-lock-app/pages/_app.tsx
+++ nft-lock-app/pages/_app.tsx
@@ -3,14 +3,16 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mumbai;
+const activeChainId = ChainId.Polygon;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log("Client ID:", process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID);
   return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
+    <ThirdwebProvider
+      activeChain={activeChainId}
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID || ""}
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
 }
-
-export default MyApp;
